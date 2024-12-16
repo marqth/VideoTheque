cf19e5785027703944288e42baf942bd4b694235
tricoito <tom.tricoire@reseau.eseo.fr>
2024-12-16 09:37:16 +0100

    Programme de base

--- VideoTheque/Program.cs
+++ VideoTheque/Program.cs
@@ -6,15 +6,18 @@ using VideoTheque.Businesses.Personnes;
 using VideoTheque.Businesses.Hosts;
 using VideoTheque.Businesses.AgeRatings;
 using VideoTheque.Businesses.Films;
-using VideoTheque.Configurations;
 using VideoTheque.Context;
 using VideoTheque.Core;
 using VideoTheque.Repositories.Genres;
 using VideoTheque.Repositories.AgeRatings;
 using VideoTheque.Repositories.Supports;
 using VideoTheque.Repositories.Films;
+
 using VideoTheque.Repositories.Personnes;
 using VideoTheque.Repositories.Hosts;
+using VideoTheque.Repositories.AgeRatings;
+using VideoTheque.Repositories.Films;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,9 +44,14 @@ builder.Services.AddScoped<IFilmsRepository, FilmsRepository>();
 builder.Services.AddScoped<IFilmsBusiness, FilmsBusiness>();
 builder.Services.AddScoped<IPersonnesRepository, PersonnesRepository>();
 builder.Services.AddScoped<IPersonnesBusiness, PersonnesBusiness>();
+builder.Services.AddScoped<IFilmsRepository, FilmsRepository>();
+builder.Services.AddScoped<IFilmsBusiness, FilmsBusiness>();
 builder.Services.AddScoped<IHostsRepository, HostsRepository>();
 builder.Services.AddScoped<IHostsBusiness, HostsBusiness>();
+builder.Services.AddScoped<IFilmsRepository, FilmsRepository>();
+builder.Services.AddScoped<IFilmsBusiness, FilmsBusiness>();
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -57,19 +65,22 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddCors(option => option
     .AddDefaultPolicy(builder => builder
-        .SetIsOriginAllowed(_ => true)
+        .SetIsOriginAllowed(_=> true)
         .AllowAnyMethod()
         .AllowAnyHeader()
         .AllowCredentials()));
 
 var app = builder.Build();
 
-// Create a scope to resolve the scoped services
-using (var scope = app.Services.CreateScope())
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    var serviceProvider = scope.ServiceProvider;
-    // Register Mapster configurations
-    MapsterConfig.RegisterMappings(serviceProvider);
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "VidéoThèque API V1");
+    });
 }
 
 app.UseRouting();
